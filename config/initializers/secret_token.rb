56951d53af3e7b37312887dbc1b005b27787bc0c
Ubuntu <ubuntu@ip-10-223-2-96.us-west-1.compute.internal>
2012-11-20 01:47:00 +0000

    Initial commit of weather_recorder app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherRecorder::Application.config.secret_token = '0fb1b3551df5ee104a90ae10aa372ab894c824693a309cc43912f460647b7dd6dd90226a29cce5f359c30cc6bf9a5cc15b43e23c0c0dea3cfaeae1bfc5b2ddf3'
